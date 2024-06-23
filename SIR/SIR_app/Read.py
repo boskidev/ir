e9c03675e2173d48c7a14c0bfa64f31dbda8a5fe
boskidev <boski.dev@gmail.com>
2024-06-23 12:01:37 +0400

    Update Read.py

--- SIR/SIR_app/Read.py
+++ SIR/SIR_app/Read.py
@@ -1,9 +1,7 @@
-import pythoncom
 from docx import Document
-import win32com.client
-import os 
+import os
+
 def read_doc_file(doc_path):
-    pythoncom.CoInitialize()  # Initialize the COM library
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     doc_path = os.path.join(BASE_DIR, 'SIR_app/Data/{0}'.format(doc_path))
 
@@ -14,15 +12,12 @@ def read_doc_file(doc_path):
             paragraphs = [paragraph.text for paragraph in doc.paragraphs]
             return '\n'.join(paragraphs)
         elif doc_path.endswith('.doc'):
-            # For .doc files, use the pywin32 library
-            word_app = win32com.client.Dispatch('Word.Application')
-            doc = word_app.Documents.Open(doc_path)
-            content = doc.Content.Text
-            doc.Close()
-            return content
+            # Handle .doc files if necessary, but note it's not recommended on Linux
+            print("Warning: Handling .doc files on Linux may not work reliably.")
+            return None
         else:
             print(f"Unsupported file format: {doc_path}")
             return None
-    finally:
-        pythoncom.CoUninitialize()  # Uninitialize the COM library
-
+    except Exception as e:
+        print(f"Error reading document {doc_path}: {e}")
+        return None
